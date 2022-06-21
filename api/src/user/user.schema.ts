8ef139fc89667d79e50eab7e965cfec7fdb4e7bd
Michael Chilaka <obichilaka@gmail.com>
2022-06-21 08:18:34 +0100

    export UserSchema variable thru SchemaFactory

--- api/src/user/user.schema.ts
+++ api/src/user/user.schema.ts
@@ -1,4 +1,6 @@
-import { Prop, Schema } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export type UserDocument = User & Document;
 
 @Schema()
 export class User {
@@ -9,3 +11,5 @@ export class User {
   @Prop({ required: true })
   password: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
