ce2b8853086cbcc93711daee308fd617c2ae7bb4
Michael Chilaka <obichilaka@gmail.com>
2022-06-24 06:18:31 +0100

    created-post-for-register-var-on-auth.controller.ts

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { NewUserDTO } from 'src/user/dtos/new-user.dtos';
+import { UserDetails } from 'src/user/user-details.interface';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() user: NewUserDTO): Promise<UserDetails | null> {
+    return this.authService.register(user);
+  }
+}
