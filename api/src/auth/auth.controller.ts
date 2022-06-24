862972654cb0e1630d82d6b317b4bc365e4c1980
Michael Chilaka <obichilaka@gmail.com>
2022-06-24 21:43:38 +0100

    login api function

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ExistingUserDTO } from 'src/user/dtos/exist-user.dtos';
 
 import { NewUserDTO } from 'src/user/dtos/new-user.dtos';
 import { UserDetails } from 'src/user/user-details.interface';
@@ -11,5 +12,11 @@ export class AuthController {
   @Post('register')
   register(@Body() user: NewUserDTO): Promise<UserDetails | null> {
     return this.authService.register(user);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  login(@Body() user: ExistingUserDTO): Promise<{ token: string } | null> {
+    return this.authService.login(user);
   }
 }
