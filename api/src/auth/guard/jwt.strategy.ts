1cc8c0e65fecbd5603546fc24f7722d2825f9b3d
Michael Chilaka <obichilaka@gmail.com>
2022-06-25 22:25:28 +0100

    bearer token for jwt

--- api/src/auth/guard/jwt.strategy.ts
+++ api/src/auth/guard/jwt.strategy.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {}
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'secret',
+    });
+  }
+}
