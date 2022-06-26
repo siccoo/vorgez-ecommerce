f84a2ece8698a07f462c74c76ab74f5ea3e1ebb4
Michael Chilaka <obichilaka@gmail.com>
2022-06-26 22:55:52 +0100

    routing to different pages

--- amazon/src/App.tsx
+++ amazon/src/App.tsx
@@ -3,12 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./shared/utils/theme";
 
+import HomePage from "./pages/Home.page";
+import RegisterPage from "./pages/Register.page";
+import SigninPage from "./pages/Signin.page";
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
         <Routes>
-          
+          <Route path="/" element={<HomePage />}  />
+          <Route path="/" element={<RegisterPage />}  />
+          <Route path="/" element={<SigninPage />}  />
         </Routes>
       </Router>
     </ThemeProvider>
