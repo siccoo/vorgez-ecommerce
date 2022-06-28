605164de33ffe19133835324f9c1efc3708907a6
Michael Chilaka <obichilaka@gmail.com>
2022-06-28 09:00:06 +0100

    updates on form fields on RegisterForm file

--- amazon/src/features/auth/components/RegisterForm.component.tsx
+++ amazon/src/features/auth/components/RegisterForm.component.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { FC, FormEvent } from 'react'
+import { Box, Grid, InputLabel, TextField, Typography } from '@mui/material'
 
 const RegisterFormComponent = () => {
   return (
-    <div>Registration Form Component</div>
+    <Box sx={{ padding: 2, border: 1, borderColor: "#CCCCCC", width: "350px", marginTop: 2 }}>
+      <form>
+        <Grid container direction="column" justifyContent="flex-start">
+          <Typography variant="h4" component="h1">Create account</Typography>
+          <InputLabel sx={{ fontWeight: 500, marginTop: 1, color: "#000000" }} htmlFor="name">Your name</InputLabel>
+          <TextField type="text" name="name" id="name" variant="outlined" size="small" />
+        </Grid>
+      </form>
+    </Box>
   )
 }
 
