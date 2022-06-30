0a892ed6f40d62cb6b77b007efad6a6f856d1831
Michael Chilaka <obichilaka@gmail.com>
2022-06-30 03:53:24 +0100

    inputReducer func updates

--- amazon/src/hooks/use-input.ts
+++ amazon/src/hooks/use-input.ts
@@ -1,6 +1,30 @@
+import { Action } from "../shared/models/action.interface";
+import {
+  InputActionType,
+  INPUT_ACTION_BLUR,
+  INPUT_ACTION_CHANGE,
+  INPUT_ACTION_CLEAR,
+} from "./models/InputAction";
 import { InputState } from "./models/InputState.interface";
 
 const initialInputState: InputState = {
   text: "",
   hasBeenTouched: false,
+};
+
+const inputReducer = (state: InputState, action: Action<InputActionType>) => {
+  const { type, value = "" } = action;
+
+  switch (type) {
+    case INPUT_ACTION_CHANGE:
+      return { text: value, hasBeenTouched: state.hasBeenTouched };
+
+    case INPUT_ACTION_BLUR:
+      return { text: state.text, hasBeenTouched: true };
+
+    case INPUT_ACTION_CLEAR:
+      return { text: "", hasBeenTouched: false };
+    default:
+      return { ...state };
+  }
 };
