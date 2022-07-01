8d2297c7ab3594e7b02f861896db662f79763cef
Michael Chilaka <obichilaka@gmail.com>
2022-07-01 12:12:08 +0100

    created RegisterFormField interface component

--- amazon/src/shared/utils/validation/email.ts
+++ amazon/src/shared/utils/validation/email.ts
@@ -1,6 +1,7 @@
 import { ValidatorFn } from "./models/ValidatorFn";
 
 export const validateEmail: ValidatorFn = (email: string): boolean => {
+  // RFC 2822
   const re =
     /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
   return re.test(email.trim());
