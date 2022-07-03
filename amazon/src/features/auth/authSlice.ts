38db3aac7152a44581cb3c22d27915007e4e010b
Michael Chilaka <obichilaka@gmail.com>
2022-07-03 23:28:16 +0100

    updates on authSlice.ts

---
+++ amazon/src/features/auth/authSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface AsyncState {
+    isLoading: boolean;
+    isSuccess: boolean;
+    isError: boolean;
+}
+
+interface AuthState extends AsyncState 
+
+const initialState: 
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState
+})
