a4088f702a70036bc78002e9ab9aca262fcb8943
Michael Chilaka <obichilaka@gmail.com>
2022-07-05 11:49:54 +0100

    created auth.service to integrate an Api & env file

---
+++ amazon/src/features/auth/services/auth.service.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { DisplayUser } from "../models/DisplayUser.interface";
+import { NewUser } from "../models/NewUser";
+
+const register = async (newUser: NewUser): Promise<DisplayUser | null> => {
+  const response = await axios.post(`${process.env.REACT_APP_BASE_API}/auth/register`);
+};
+
+export const authService = {
+  // register,
+  // login,
+  // logout,
+  // verifyJwt
+};
