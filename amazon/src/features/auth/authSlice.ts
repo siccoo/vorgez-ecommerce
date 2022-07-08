0e2be83580c72657bb0bd1f02151e0519a9b70c7
Michael Chilaka <obichilaka@gmail.com>
2022-07-08 21:26:40 +0100

    storedUser/user & storedJwt/jwt variables added

--- amazon/src/features/auth/authSlice.ts
+++ amazon/src/features/auth/authSlice.ts
@@ -5,6 +5,12 @@ import { Jwt } from "./models/Jwt";
 import { NewUser } from "./models/NewUser";
 import { authService } from "./services/auth.service";
 
+const storedUser: string | null = localStorage.getItem('user');
+const user: DisplayUser | null = !!storedUser ? JSON.parse(storedUser) : null;
+
+const storedJwt: string | null = localStorage.getItem('jwt');
+const jwt: Jwt = !!storedJwt ? JSON.parse(storedJwt) : null;
+
 interface AsyncState {
   isLoading: boolean;
   isSuccess: boolean;
