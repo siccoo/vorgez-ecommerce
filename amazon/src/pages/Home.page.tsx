cf70dcef5ac688fe2cd39961520356b72da9881d
Michael Chilaka <obichilaka@gmail.com>
2022-07-10 07:53:13 +0100

    logout func on Home component

--- amazon/src/pages/Home.page.tsx
+++ amazon/src/pages/Home.page.tsx
@@ -1,6 +1,29 @@
+import { logout } from "../features/auth/authSlice";
+import { useAppDispatch, useAppSelector } from "../hooks/redux/hooks";
+
 const HomePage = () => {
+  const dispatch = useAppDispatch();
+
+  const { user } = useAppSelector((state) => state.auth);
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
-    <div>Home page</div>
+    <div>
+      <h1>Home page</h1>
+      <a onClick={logoutHandler}
+        style={{
+          backgroundColor: "yellow",
+          cursor: "pointer",
+          width: "60px",
+          height: "40px",
+          padding: "8px",
+          // float: "right",
+        }}>Logout</a>
+      {user?.email}
+    </div>
   )
 }
 
