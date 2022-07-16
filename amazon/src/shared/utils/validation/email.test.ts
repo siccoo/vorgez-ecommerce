71cb8c0172b204722536cd87c3a02f915278b272
Michael Chilaka <obichilaka@gmail.com>
2022-07-16 07:05:49 +0100

    unit test on pass email validation

--- amazon/src/shared/utils/validation/email.test.ts
+++ amazon/src/shared/utils/validation/email.test.ts
@@ -5,5 +5,18 @@ describe('Email validation', () => {
 
     test('an empty input should not be valid', () => {
         expect(validateEmail(email)).toEqual(false);
-    })
+    });
+
+    test('it should have an @ symbol', () => {
+        email = '';
+        expect(email.includes('@')).toEqual(false);
+    });
+
+    test('it should have a . symbol', () => {
+        expect(email.includes('.')).toEqual(false);
+    });
+
+    test('a valid email should pass validation', () => {
+        expect(validateEmail(email)).toEqual(false);
+    });
 })
