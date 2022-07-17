97e8833577fd989c8c341be6d5a80d19883c9d37
Michael Chilaka <obichilaka@gmail.com>
2022-07-17 07:06:12 +0100

    unit test on name length passes 2 or is 2 characters

--- amazon/src/shared/utils/validation/length.test.ts
+++ amazon/src/shared/utils/validation/length.test.ts
@@ -6,6 +6,16 @@ describe('Field length validation', () => {
         test('a name should fail length validation if it is not set', () => {
             expect(validateNameLength(name)).toEqual(false);
         });
+
+        test('a name should fail length validation if it is less than 2', () => {
+            name = 'J'
+            expect(validateNameLength(name)).toEqual(false);
+        });
+
+        test('a name should pass length validation if it is 2 characters', () => {
+            name = 'Jo'
+            expect(validateNameLength(name)).toEqual(true);
+        });
     })
     
 
