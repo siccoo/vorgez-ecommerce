bb4aefe267f0ca5e4a49400b102f437805542e3e
Michael Chilaka <obichilaka@gmail.com>
2022-07-27 06:14:09 +0100

    continuation of cypress e2e testing

--- amazon/src/App.tsx
+++ amazon/src/App.tsx
@@ -8,19 +8,23 @@ import RegisterPage from "./pages/Register.page";
 import SigninPage from "./pages/Signin.page";
 import PrivateRoute from "./features/auth/components/PrivateRoute";
 
+import { store } from "./store"
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
         <Routes>
-          <Route path="/" element={<PrivateRoute page={<HomePage />} />}  />
-          <Route path="/register" element={<RegisterPage />}  />
-          <Route path="/signin" element={<SigninPage />}  />
+          <Route path="/" element={<PrivateRoute page={<HomePage />} />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/signin" element={<SigninPage />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
     </ThemeProvider>
   );
 }
+
+type CypressWindow = Window & typeof globalThis & { Cypress: any; store: any };
 
 export default App;
