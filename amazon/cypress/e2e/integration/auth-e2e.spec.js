294e98c2e3dbc1b47af523914cc5f3e266d83512
Michael Chilaka <obichilaka@gmail.com>
2022-07-27 12:38:50 +0100

    setup with the store state on cypress

--- amazon/cypress/e2e/integration/auth-e2e.spec.js
+++ amazon/cypress/e2e/integration/auth-e2e.spec.js
@@ -1,22 +1,27 @@
 /* eslint-disable no-undef */
 describe('Auth (e2e)', () => {
-    it('should load and redirect to /signin', () => {
-      cy.visit('http://localhost:3001/');
-  
-      cy.url().should('include', 'signin');
-    });
-  
-    it('should have default initial state', () => {
-      const initialAppState = {
-        auth: {
-          user: null,
-          jwt: null,
-          isAuthenticated: false,
-          isLoading: false,
-          isSuccess: false,
-          isError: false,
-        },
-      };
-    });
+  it('should load and redirect to /signin', () => {
+    cy.visit('http://localhost:3001/');
 
+    cy.url().should('include', 'signin');
   });
+
+  it('should have default initial state', () => {
+    const initialAppState = {
+      auth: {
+        user: null,
+        jwt: null,
+        isAuthenticated: false,
+        isLoading: false,
+        isSuccess: false,
+        isError: false,
+      },
+    };
+
+    cy.window()
+      .its('store')
+      .invoke('getState')
+      .should('deep.equal', initialAppState);
+  });
+
+});
