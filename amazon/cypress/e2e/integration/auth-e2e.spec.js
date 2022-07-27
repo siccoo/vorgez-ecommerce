f0f584c831964aa946114e2436f3931550c91405
Michael Chilaka <obichilaka@gmail.com>
2022-07-27 07:08:19 +0100

    storing data in the global Cypress window

---
+++ amazon/cypress/e2e/integration/auth-e2e.spec.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+describe('Auth (e2e)', () => {
+    it('should load and redirect to /signin', () => {
+      cy.visit('http://localhost:3001/');
+  
+      cy.url().should('include', 'signin');
+    });
+  
+    it('should have default initial state', () => {
+      const initialAppState = {
+        auth: {
+          user: null,
+          jwt: null,
+          isAuthenticated: false,
+          isLoading: false,
+          isSuccess: false,
+          isError: false,
+        },
+      };
+    });
+
+  });
