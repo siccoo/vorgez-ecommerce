d84ef6f8efab47dfd37da3a80752306624d0d239
Michael Chilaka <obichilaka@gmail.com>
2022-08-01 14:26:13 +0100

    updates on productSlice.ts

---
+++ amazon/src/features/product/productSlice.ts
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { ProductDocument } from "../product/models/Product";
+import { Cart } from "./models/Cart";
+import productService from "./services/product.service";
+
+export {};
+
+interface AsyncState {
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+}
+
+interface ProductState extends AsyncState {
+  products: ProductDocument[];
+  cart: Cart;
+}
+
+const initialState: ProductState = {
+  products: [],
+  cart: [],
+  isLoading: false,
+  isSuccess: false,
+  isError: false,
+};
+
+export const getProducts = createAsyncThunk('product', 
+async () => {
+    try {
+       return await productService.getProducts(); 
+    } catch (error) {
+        console.log('Error: ', error);
+        
+    }
+});
+
+export const productSlice = createSlice({
+    name: 'product',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        
+    }
+})
