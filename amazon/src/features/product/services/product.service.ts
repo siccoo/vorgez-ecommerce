8a22813bb04407a9226420e34344d2222f7333ef
Michael Chilaka <obichilaka@gmail.com>
2022-08-01 07:54:59 +0100

    getProduct function

---
+++ amazon/src/features/product/services/product.service.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { ProductDocument } from "../models/Product";
+
+const getProducts = async () => {
+  const response = await axios.get<ProductDocument[]>(
+    `${process.env.REACT_APP_BASE_API}/product`
+  );
+
+  return response;
+};
