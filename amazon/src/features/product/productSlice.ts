cb9f2f1a3ab0453c0b26fcd59633d7dae4968052
Michael Chilaka <obichilaka@gmail.com>
2022-08-02 01:57:21 +0100

    updates on slice reducer on products

--- amazon/src/features/product/productSlice.ts
+++ amazon/src/features/product/productSlice.ts
@@ -24,21 +24,34 @@ const initialState: ProductState = {
   isError: false,
 };
 
-export const getProducts = createAsyncThunk('product', 
-async () => {
-    try {
-       return await productService.getProducts(); 
-    } catch (error) {
-        console.log('Error: ', error);
-        
-    }
+export const getProducts = createAsyncThunk("product", async () => {
+  try {
+    return await productService.getProducts();
+  } catch (error) {
+    console.log("Error: ", error);
+  }
 });
 
 export const productSlice = createSlice({
-    name: 'product',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        
-    }
-})
+  name: "product",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.products = action.payload?.data || [];
+      })
+      .addCase(getProducts.rejected, (state) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.products = [];
+      })
+  },
+});
+
+export default productSlice.reducer;
--- amazon/src/store.ts
+++ amazon/src/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import authReducer from "./features/auth/authSlice"
+import authReducer from "./features/auth/authSlice";
+import productReducer from "./features/product/productSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        product: productReducer
     }
 })
 
