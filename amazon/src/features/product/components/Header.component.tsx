68d57d61c3b575f0811aa1fee9a92092904222b4
Michael Chilaka <obichilaka@gmail.com>
2022-08-07 05:07:21 +0100

    updates on HeaderComponent.tsx

--- amazon/src/features/product/components/Header.component.tsx
+++ amazon/src/features/product/components/Header.component.tsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react";
+
+import { AppBar, Badge, Box, Button, Toolbar } from "@mui/material";
+// import { ShoppingBagOutlinedIcon } from "@mui/icons-material/ShoppingBagOutlined";
+
 import { useNavigate } from "react-router-dom";
 import { logout, selectedUser } from "../../../features/auth/authSlice";
 import { useAppDispatch, useAppSelector } from "../../../hooks/redux/hooks";
@@ -6,7 +10,7 @@ import { useAppDispatch, useAppSelector } from "../../../hooks/redux/hooks";
 const HeaderComponent = () => {
 
   const { user } = useAppSelector(selectedUser);
-  const cart = useAppSelector((state) => state.product);
+  const { cart } = useAppSelector((state) => state.product);
 
   const [cartCount, setCartCount] = useState(0);
 
@@ -15,7 +19,8 @@ const HeaderComponent = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const totalQty = cart.reduce((acc: any, item: { quantity: any; }) => acc + item.quantity, 0);
+    const totalQty = cart.reduce((acc, item) => acc + item.quantity, 0);
+    setCartCount(() => totalQty);
   }, [user])
 
 
@@ -23,8 +28,17 @@ const HeaderComponent = () => {
     dispatch(logout())
   }
   return (
-    <div>
-      <h1>Home page</h1>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{
+        backgroundColor:
+          '#131921', color: '#ffffff', padding: '4px'
+      }}>
+        <Toolbar sx={{
+          backgroundColor:
+            '#131921', color: '#ffffff', padding: '4px'
+        }}></Toolbar>
+      </AppBar>
+      {/* <h1>Home page</h1>
       <a onClick={logoutHandler}
         style={{
           backgroundColor: "yellow",
@@ -34,8 +48,8 @@ const HeaderComponent = () => {
           padding: "8px",
           // float: "right",
         }}>Logout</a>
-      {user?.email}
-    </div>
+      {user?.email} */}
+    </Box>
   )
 }
 
