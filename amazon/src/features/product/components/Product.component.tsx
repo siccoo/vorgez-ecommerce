079a176e87b7fea9eb242d110f54d0c7775290fe
Michael Chilaka <obichilaka@gmail.com>
2022-08-08 12:18:36 +0100

    increment/decrement Product items added

--- amazon/src/features/product/components/Product.component.tsx
+++ amazon/src/features/product/components/Product.component.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from 'react';
 import { ProductDocument } from '../models/Product';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from "@mui/material"
 import { useAppDispatch } from 'src/hooks/redux/hooks';
+import { decrementProduct, incrementProduct } from '../productSlice';
 
 interface ProductComponentProps {
     product: ProductDocument
@@ -33,12 +34,25 @@ const ProductComponent: FC<ProductComponentProps> = ({ product }) => {
                     onClick={() => {
                         setCount((prevCount: number) => {
                             if (prevCount === 0) return 0;
-                            return prevCount - 1
+                            return prevCount - 1; 
                         });
+                        dispatch(decrementProduct(product));
                     }}
                     disabled={count === 0}
                 >
                     -
+                </Button>
+                <span>{count}</span>
+                <Button
+                    onClick={() => {
+                        setCount((prevCount: number) => 
+                            prevCount + 1 
+                        );
+                        dispatch(incrementProduct(product));
+                    }}
+                    disabled={count === 0}
+                >
+                    +
                 </Button>
             </CardActions>
         </Card>
