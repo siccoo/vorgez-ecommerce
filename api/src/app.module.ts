b9f20194d91345e4a7c418e0a798cd10379bf1a7
Michael Chilaka <obichilaka@gmail.com>
2022-08-08 23:18:46 +0100

    create & update of stripe module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,6 +6,7 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { StripeModule } from './stripe/stripe.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from './auth/auth.module';
     ProductModule,
     UserModule,
     AuthModule,
+    StripeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
