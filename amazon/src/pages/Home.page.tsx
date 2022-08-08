0633a52d8e46ccd4775330705e45028596af6d84
Michael Chilaka <obichilaka@gmail.com>
2022-08-08 12:30:54 +0100

    mapping all products in the HomePage.tsx

--- amazon/src/pages/Home.page.tsx
+++ amazon/src/pages/Home.page.tsx
@@ -1,4 +1,5 @@
 import HeaderComponent from "src/features/product/components/Header.component"
+import ProductComponent from "src/features/product/components/Product.component";
 import { useAppDispatch, useAppSelector } from "src/hooks/redux/hooks";
 
 const HomePage = () => {
@@ -8,6 +9,20 @@ const HomePage = () => {
   return (
     <div>
       <HeaderComponent />
+      <div
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: '48px',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '40px'
+      }}
+      >
+        {products.length > 0 && products.map((product) => (
+          <ProductComponent key={product._id} product={product} />
+        ))}
+      </div>
     </div>
   )
 }
