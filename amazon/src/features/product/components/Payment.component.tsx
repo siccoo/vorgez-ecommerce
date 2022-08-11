39a4b748440ace6bf130eaa0bec22bb9bc759dbb
Michael Chilaka <obichilaka@gmail.com>
2022-08-11 16:20:32 +0100

    updates on Payment Gateway component

--- amazon/src/features/product/components/Payment.component.tsx
+++ amazon/src/features/product/components/Payment.component.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements, CardElement, useStripe, useElements, } from "@stripe/react-stripe-js"
 import { useAppDispatch, useAppSelector } from "src/hooks/redux/hooks";
+import { resetCart } from "../productSlice";
 
 
 const PaymentComponent = () => {
@@ -18,8 +19,42 @@ const PaymentComponent = () => {
     const stripe = useStripe();
     const elements = useElements();
 
+    useEffect(() => {
+        if (totalQty === 0) return;
+
+        if (paymentStatus !== 'succeeded') return;
+
+        dispatch(resetCart());
+    })
+
     return (
-        <div>Payment.component</div>
+        <div style={{ fontSize: '20px' }}>
+            <form id="payment-form">
+                <label htmlFor="card-element">Place order</label>
+                <CardElement id="card-element" />
+                {!isProcessing && (
+                    <button
+                        style={{
+                            marginTop: '16px',
+                            height: '31px', 
+                            border: 'none',
+                            backgroundColor: '#47A0F1',
+                            color: '#000000',
+                            display: 'flex',
+                            fontWeight: 600,
+                            fontSize: '20px',
+                            padding: '24px',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            cursor: 'pointer',
+                            width: '100%'
+                        }}
+                    >Pay</button>
+                )}
+                {isProcessing && <div>Processing...</div>}
+                {!isProcessing && paymentStatus && <div>{paymentStatus}</div>}
+            </form>
+        </div>
     )
 }
 
